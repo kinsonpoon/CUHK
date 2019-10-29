48b24fee6bc93a165d9e59931cccc1c0d4590dfd
kinsonp <kinson.poon@gmail.com>
2019-10-29 12:50:42 +0800

    new

--- GIS-FYP/clp.py
+++ GIS-FYP/clp.py
@@ -31,6 +31,123 @@ def makedf(lx,ly,lh,lmap,lgp,name):
     zippedList =  list(zip(lx,ly,lh,lmap,lgp))
     df = pd.DataFrame(zippedList, columns = ['x' , 'y','H','map','gp']) 
     df.to_csv(filename,index=False) 
+def get_txt(msp,dfile):
+    lx=[]
+    ly=[]
+    lt=[]
+    lmap=[]
+    lgp=[]
+    for mtext in msp.query("MTEXT"):
+        t=mtext.text
+        
+        #print(t)
+        #print(mtext.dxf.insert)
+        if(mtext.dxf.insert[2]==0.0 and t.replace('.','',1).isdigit() and int(t)%10==0):
+            lt.append(mtext.text)
+            lx.append(mtext.dxf.insert[0])
+            ly.append(mtext.dxf.insert[1])
+            lmap.append(dfile)
+            lgp.append("X")
+        elif(t.isalpha() and ord(mtext.text[0])!=92 and t!="Ruin" and t!="Fence" and t!="Pipeline"):
+            lt.append(mtext.text)
+            lx.append(mtext.dxf.insert[0])
+            ly.append(mtext.dxf.insert[1])
+            lmap.append(dfile)
+            lgp.append("X")
+    makedf(lx,ly,lt,lmap,lgp,"char")
+    
+def RKA(entities):
+    lx=[]
+    ly=[]
+    lh=[]
+    lmap=[]
+    lgp=[]
+    j=0
+    for entity in entities:
+        if(entity.dxftype()=="LWPOLYLINE"):
+            pt=entity.get_points()
+            length=len(pt)
+            if(length>50):
+                for i in range(0,length,2):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0]not in lx:
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            elif(length>200):
+                for i in range(0,length,4):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0] not in lx :
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            else:
+                for x in pt:
+                    lx.append(round(x[0],1))
+                    ly.append(round(x[1],1))
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            j=j+1
+    makedf(lx,ly,lh,lmap,lgp,"RKA") 
+def HW(entities):
+    lx=[]
+    ly=[]
+    lh=[]
+    lmap=[]
+    lgp=[]
+    j=0
+    for entity in entities:
+        if(entity.dxftype()=="LWPOLYLINE"):
+            pt=entity.get_points()
+            length=len(pt)
+            if(length>50):
+                for i in range(0,length,2):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0]not in lx:
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            elif(length>200):
+                for i in range(0,length,4):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0] not in lx :
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            else:
+                for x in pt:
+                    lx.append(round(x[0],1))
+                    ly.append(round(x[1],1))
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            j=j+1
+    makedf(lx,ly,lh,lmap,lgp,"HW") 
 def ICL(entities):
     lx=[]
     ly=[]
@@ -49,7 +166,20 @@ def ICL(entities):
                     lmap.append(dfile)
                     lh.append("NA")
                     lgp.append(j)
-                if(length%2==0):
+                if pt[length-1][0]not in lx:
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            elif(length>200):
+                for i in range(0,length,4):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0] not in lx :
                     lx.append(pt[length-1][0])
                     ly.append(pt[length-1][1])
                     lmap.append(dfile)
@@ -82,7 +212,20 @@ def NCL(entities):
                     lmap.append(dfile)
                     lh.append("NA")
                     lgp.append(j)
-                if(length%2==0):
+                if pt[length-1][0]not in lx:
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            elif(length>200):
+                for i in range(0,length,4):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0] not in lx :
                     lx.append(pt[length-1][0])
                     ly.append(pt[length-1][1])
                     lmap.append(dfile)
@@ -98,13 +241,77 @@ def NCL(entities):
             j=j+1
     makedf(lx,ly,lh,lmap,lgp,"NCL")
 def FP(entities):
-    
+    lx=[]
+    ly=[]
+    lh=[]
+    lmap=[]
+    lgp=[]
+    j=0
+    for entity in entities:
+        if(entity.dxftype()=="LWPOLYLINE"):
+            pt=entity.get_points()
+            length=len(pt)
+            if(length>50):
+                for i in range(0,length,2):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0]not in lx:
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            elif(length>200):
+                for i in range(0,length,4):
+                    lx.append(pt[i][0])
+                    ly.append(pt[i][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+                if pt[length-1][0] not in lx :
+                    lx.append(pt[length-1][0])
+                    ly.append(pt[length-1][1])
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+            else:
+                for x in pt:
+                    lx.append(round(x[0],1))
+                    ly.append(round(x[1],1))
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+        else:
+            pt=[]
+            pt.append(entity.dxf.start)
+            pt.append(entity.dxf.end)
+            for x in pt:
+                    lx.append(round(x[0],1))
+                    ly.append(round(x[1],1))
+                    lmap.append(dfile)
+                    lh.append("NA")
+                    lgp.append(j)
+        j=j+1
+        
+    makedf(lx,ly,lh,lmap,lgp,"FP")
 def tran(group,dfile):
     for layer, entities in group.items():
         if(layer=='ICL'):
             ICL(entities)
         if(layer=='NCL'):
            NCL(entities)
+        if(layer=='FP'):
+            FP(entities)
+        if(layer=="cwf_f"):
+            for entity in entities:
+                print(entity.dxftype())
+            #HW(entities)
+        if(layer=="RKA"):
+            RKA(entities)
+    
 path="C:\\Users\\kinsonp\\Desktop\\FYP\\2011"
 dfile="3seb"
 targetfile=dfile+".dxf"
@@ -114,9 +321,10 @@ doc = ezdxf.readfile(dxf)
 msp = doc.modelspace()
 
 
-
+#get_txt(msp,dfile)
 group = msp.groupby(dxfattrib='layer')
 tran(group,dfile)
+
 '''
 for layer, entities in group.items():
     if(layer=='NCL'):
--- GIS-FYP/svg/draw.py
+++ GIS-FYP/svg/draw.py
@@ -16,24 +16,50 @@ def poly_list(df):
             temp.append((df['x'][i],df['y'][i]))
     data.append(temp)
     return data
+def text_list(df):
+    temp=[]
+    data=[]
+    for i in range(len(df['x'])):
+        data.append((df['x'][i],df['y'][i],df['H'][i]))
+    #print(data)
+    return data
 Nfile="NCL3seb_proj.csv"
 Cfile="ICL3seb_proj.csv"
+Ffile="FP3seb_proj.csv"
+Tfile="char3seb_proj.csv"
+file1="CL3seb_proj.csv"
+file2="RKA3seb_proj.csv"
 nlp = pd.read_csv(Nfile, header = 0)
 clp = pd.read_csv(Cfile, header = 0)
+FP = pd.read_csv(Ffile, header = 0)
+T=pd.read_csv(Tfile, header = 0)
+f1=pd.read_csv(file1, header = 0)
+f2=f1=pd.read_csv(file2, header = 0)
 map=nlp['map'][0]
 sname=str(map)+".svg"
 
 data=poly_list(nlp)
 data2=poly_list(clp)
-
+data3=poly_list(FP)
+data4=text_list(T)
+data5=poly_list(f1)
+data6=poly_list(f2)
 
 svg_document = svgwrite.Drawing(filename = sname,size = ("937.5px", "750px"))
 for d in data:
-    svg_document.add(svg_document.polyline(d,stroke='blue', fill='none'))
+    svg_document.add(svg_document.polyline(d,stroke=svgwrite.rgb(189, 94, 0, '%'), fill='none'))
 for d in data2:
-    svg_document.add(svg_document.polyline(d,stroke='red', fill='none'))
+    svg_document.add(svg_document.polyline(d,stroke=svgwrite.rgb(129, 64, 0, '%'), fill='none'))
+for d in data3:
+    svg_document.add(svg_document.polyline(d,stroke=svgwrite.rgb(79, 64,79, '%'), fill='none'))
+for d in data5:
+    svg_document.add(svg_document.polyline(d,stroke=svgwrite.rgb(127, 255,0, '%'), fill='none'))
+for d in data6:
+    svg_document.add(svg_document.polyline(d,stroke=svgwrite.rgb(0, 94,189, '%'), fill='none'))
+for d in data4:
+    svg_document.add(svg_document.text(d[2],insert = (d[0], d[1]),font_size="3px",fill='black'))
 
 
-print(svg_document.tostring())
+#print(svg_document.tostring())
 
 svg_document.save()
